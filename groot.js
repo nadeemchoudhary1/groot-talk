845ba0cf2ead258399e8981b671eb82c7d6580a9
mohd-nadeem <nadeem7210357259@gmail.com>
2022-12-23 16:58:03 +0530

    code added

---
+++ groot.js
@@ -0,0 +1,33 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// this is my url, for groot
+
+var serverURL = "https://api.funtranslations.com/translate/groot.json";
+
+function getTranslationURL(text){
+    return serverURL + "?text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+}
+
+function clickHandler(){
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(Response => Response.json())
+    .then(json => {
+        if(json.contents){
+            var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        }
+        else{
+            outputDiv.innerText = json.error.message;   
+        }
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
